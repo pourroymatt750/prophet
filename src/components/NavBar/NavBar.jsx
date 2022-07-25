fe5316ac4394a167130b610856a9db58c835b43d
Matthew Pourroy <pourroy.matt@gmail.com>
2022-07-25 12:12:12 -0400

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,28 @@
 import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
-          </ul>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Prophet</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/unit-one">Unit One</Nav.Link>
+              <Nav.Link href="/unit-two">Unit Two</Nav.Link>
+              <Nav.Link href="/unit-three">Unit Three</Nav.Link>
+              <Nav.Link href="/unit-four">Unit Four</Nav.Link>
+              <Nav.Link href="/profiles">Community</Nav.Link>
+              <Nav.Link href="/" onClick={handleLogout}>Log Out</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       :
         <nav>
           <ul>
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,24 @@
 import styles from './Landing.module.css'
+import { Link } from 'react-router-dom'
 
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>Welcome to Prophet!</h1>
+      <h2>A guide to the most commonly asked questions</h2>
+      {user ? 
+        ''
+        :
+        <>
+        <h2>Please login or sign up to continue</h2>
+          <Link to="/login">
+            <button className='login'>Login</button>
+          </Link>
+          <Link to="/signup">
+            <button className='signup'>Sign Up</button>
+          </Link>
+        </>
+      }
     </main>
   )
 }
